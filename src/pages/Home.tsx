e5a53d775120bdfb35bee5be695a0736521529d9
VKURHT <victork113kv@gmail.com>
2024-11-13 18:27:58 -0300

    Portfolio Finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import './CssPages/Home.css';
 import imagem from '../pages/imagensPages/FotoPerfil.jpg';
 import { useState } from 'react';
 import Navbar from '../Components/Navbar/Navbar';
-import Profile from '../Components/Profile/Profile'
-
+import Profile from '../Components/Profile/Profile';
+import Curriculo from './imagensPages/Currículo Vítor Kurth.pdf'
 
 function Home() {
   const originalText = `Olá! Meu nome é Vítor Kurth, tenho 17 anos, nasci em Florianópolis e sou estudante do curso técnico em Análise e Desenvolvimento de Sistemas no Senai.\n\nDesde o meu primeiro contato com um computador, me interessei por tecnologia, programação e jogos, o que me levou a buscar entender mais sobre esse mundo. Por isso, escolhi estudar no Senai.\n\nAlém de tecnologia, há muitas outras coisas que gosto de fazer, como viajar e conhecer culturas diferentes, sair e me divertir com os meus amigos, escutar música e ver filmes e séries e passar horas jogando os jogos que eu amo.\n\nEsse é um breve resumo sobre mim e meu interesse por tecnologia. Espero que aproveite meu portfólio!`;
@@ -37,9 +37,14 @@ function Home() {
             <span key={index}>{line}<br /></span>
           ))}</p>
         )}
+        <div className='botoes'>
         <button className='habilidades' onClick={handleClick}>
-          {buttonText}
+          Mais
         </button>
+        <button className='habilidades' onClick={() => window.open(Curriculo)}>
+          Curriculo
+        </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/PPE.tsx
+++ src/pages/PPE.tsx
@@ -1,15 +1,22 @@
 import { Link } from 'react-router-dom';
+import './CssPages/PPE.css'
 import Navbar from '../Components/Navbar/Navbar';
-
+import Certificado from '../pages/imagensPages/certificadoMec.pdf'
 
 const PPE = () => {
   return (
   
     <div className="container">
-      <div className="background-image">
-
-      </div>
+     
      <Navbar/>
+     <div className="cardAtividade">
+     <h2 className="tituloAtividade">Certificado MEC</h2>
+
+          <embed className="FileAtividade" src={Certificado}  style={{width: '100%', height:'80%'}}/>
+            
+          
+            <button className="abrirAtividade" onClick={() => window.open(Certificado)}>Certificado</button>
+          </div>
       </div>
    
   );
