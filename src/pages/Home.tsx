8784723f81665db8cbbc985783c7e4841dc8d28d
VKURHT <victork113kv@gmail.com>
2024-11-23 13:40:45 -0300

    deleted images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import imagem from '../pages/imagensPages/FotoPerfil.jpg';
 import { useState } from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import Profile from '../Components/Profile/Profile';
-import Curriculo from './imagensPages/Currículo Vítor Kurth.pdf'
 
 function Home() {
   const originalText = `Olá! Meu nome é Vítor Kurth, tenho 17 anos, nasci em Florianópolis e sou estudante do curso técnico em Análise e Desenvolvimento de Sistemas no Senai.\n\nDesde o meu primeiro contato com um computador, me interessei por tecnologia, programação e jogos, o que me levou a buscar entender mais sobre esse mundo. Por isso, escolhi estudar no Senai.\n\nAlém de tecnologia, há muitas outras coisas que gosto de fazer, como viajar e conhecer culturas diferentes, sair e me divertir com os meus amigos, escutar música e ver filmes e séries e passar horas jogando os jogos que eu amo.\n\nEsse é um breve resumo sobre mim e meu interesse por tecnologia. Espero que aproveite meu portfólio!`;
